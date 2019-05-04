c528c62c27a5a9aa090ab9dd2bf8dab1e3b72f8a
emicuenca <emi_cuenca@hotmail.com>
2019-05-04 14:17:24 -0300

    Ejercicio 2: Modificado classifier

--- sentiment/classifier.py
+++ sentiment/classifier.py
@@ -3,7 +3,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-
+from nltk.corpus import stopwords
+import re
 
 classifiers = {
     'maxent': LogisticRegression,
@@ -11,6 +12,43 @@ classifiers = {
     'svm': LinearSVC,
 }
 
+negations = ['no', 'ni', 'nunca', 'jamas', 'pero', 'tampoco']
+
+def customPreprocess(s):
+    vowelRegex = "([aeiou])\\1{2,}"
+    urlRegex = "https?://[^\\s\\<]+"
+    mentionRegex = "(?:(?<=\\s|\\>)|(?<=^))@[a-zA-Z0-9_\\-]{,15}\\s?"
+
+    # Default preprocessor converts all characters to lowercase
+    prepro = s.lower()
+    prepro = re.sub(vowelRegex, "\\1", prepro)
+    prepro = re.sub(urlRegex, " ", prepro)
+    prepro = re.sub(mentionRegex, " ", prepro)
+    return prepro
+
+class NegationTokenizer(object):
+    def __init__(self):
+        self.remaining = 0
+        self.words_after_negation = 3
+        self.negations = negations
+    
+    def __call__(self, tokens):
+        for token in tokens:
+            if 0 < self.remaining:
+                if len(token) == 1 or token in self.negations:
+                    self.remaining = 0
+                else:
+                    token =  "NOT_" + token
+                    self.remaining -= 1
+            elif token in self.negations:
+                self.remaining = self.words_after_negation
+            yield token
+
+class CustomVectorizer(CountVectorizer):
+    def build_tokenizer(self):
+        tokenize = super(CustomVectorizer, self).build_tokenizer()
+        negationTokenizer = NegationTokenizer()
+        return lambda doc: list(negationTokenizer(tokenize(doc)))
 
 class SentimentClassifier(object):
 
@@ -19,8 +57,15 @@ class SentimentClassifier(object):
         clf -- classifying model, one of 'svm', 'maxent', 'mnb' (default: 'svm').
         """
         self._clf = clf
+        stop_words = list(set(stopwords.words('spanish')) - set(negations))
         self._pipeline = pipeline = Pipeline([
-            ('vect', CountVectorizer()),
+
+            ('vect', CustomVectorizer(binary=True, preprocessor=customPreprocess, stop_words=stop_words)),
+            #('vect', CountVectorizer(stop_words=stopwords.words('spanish'))),
+            #('vect', CountVectorizer(preprocessor=customPreprocess)),
+            #('vect', CustomVectorizer()),
+            #('vect', CountVectorizer(binary=True)),
+            #('vect', CountVectorizer()),
             ('clf', classifiers[clf]()),
         ])
 
@@ -29,3 +74,6 @@ class SentimentClassifier(object):
 
     def predict(self, X):
         return self._pipeline.predict(X)
+    
+    def predict_proba(self, X):
+        return self._pipeline.predict_proba(X)
