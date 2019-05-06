0eab47398bd4835864c3b15859dbffd1686631f2
emicuenca <emi_cuenca@hotmail.com>
2019-05-06 10:27:29 -0300

    Ejercicio 5: agregado features.py

---
+++ sentiment/scripts/features.py
@@ -0,0 +1,35 @@
+"""Show most relevant features in a maxent model
+
+Usage:
+  features.py -m <file>
+  features.py -h | --help
+
+Options:
+  -m <file>     Trained model file.
+  -h --help     Show this screen.
+"""
+
+from docopt import docopt
+import pickle
+import pandas as pd
+from sentiment.analysis import print_maxent_features
+
+
+def main():
+    # Load model from file
+    opts = docopt(__doc__)
+    filename = opts['-m']
+    f = open(filename, 'rb')
+    model = pickle.load(f)
+    f.close()
+
+    # Get vectorizer and classifier
+    pipeline = model._pipeline
+    vect = pipeline.named_steps['vect']
+    clf = pipeline.named_steps['clf']
+    
+    # Print 10 most relevant features
+    print_maxent_features(vect, clf, n=10)
+
+if __name__ == '__main__':
+    main()
